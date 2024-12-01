3f1222f978385507ee0b22690df7a5aa8aefe57a
David Hernandez <davidmosquera@uninorte.edu.co>
2024-12-01 15:29:06 -0500

    added logo,period,etc

--- src/app/dashboard/component/navbar/navbar.component.ts
+++ src/app/dashboard/component/navbar/navbar.component.ts
@@ -3,13 +3,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import {provideNativeDateAdapter} from '@angular/material/core'
+import { MatSelectModule } from '@angular/material/select';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgOptimizedImage, MatDatepickerModule, MatInputModule, MatFormFieldModule, FormsModule, ReactiveFormsModule],
+  imports: [NgOptimizedImage, MatIconModule,MatDatepickerModule, MatSelectModule,MatInputModule, MatFormFieldModule, FormsModule, ReactiveFormsModule],
   providers: [provideNativeDateAdapter()],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
